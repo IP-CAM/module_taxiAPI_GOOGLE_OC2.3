6fe912bf3167de98ad2ed4b14eb49a640fec24a7
nezzo <lestatx@yandex.ru>
2017-06-21 23:00:37 +0300

    21.06.2017

--- catalog/controller/extension/module/taxiAPI.php
+++ catalog/controller/extension/module/taxiAPI.php
@@ -113,6 +113,26 @@ class ControllerExtensionModuleTaxiAPI extends Controller {
                    "to_whomPhone"   =>$to_whomPhone,
                    "payment"        =>$payment
                );
+         * 
+         *  //создаем массив по даным откуда
+        $arrayDataWhence = array(
+            "whenceCity"    =>$whenceCity,
+            "whenceStreet"  =>$whenceStreet,
+            "whenceHouse"   =>$whenceHouse,
+            "whenceExtr"    =>$whenceExtr,
+            "tarif"         =>$tarif,
+         * 
+         * 
+            
+        );
+         * 
+         * //создаем массив по даным куда
+        $arrayDataWhere = array(
+            "whereCity"     =>$whereCity,
+            "whereStreet"   =>$whereStreet,
+            "whereHouse"    =>$whereHouse,
+            "whereExtr"     =>$whereExtr
+        );
 
 
 
@@ -126,9 +146,64 @@ class ControllerExtensionModuleTaxiAPI extends Controller {
          * 
          *          */
         
+        
+        
+        #TODO надо пофиксить баги: в заказе не видно товар, не добавляется СУММА, надо создать   
+        #расчет  по ликпею если все гуд то перекидывать на success. Так же сделать  по оплате 
+        #наличными.
+        
         //если все гуд и мы получаем сумму то мы  начинаем оформлять заказ
         if(!empty((int)$km)){
+            $this->load->model('checkout/order');
+            $this->load->language('extension/payment/cheque');
+            $comment  = $this->language->get('text_payable') . "\n";
+            $comment .= $this->config->get('cheque_payable') . "\n\n";
+            $comment .= $this->language->get('text_address') . "\n";
+            $comment .= $this->config->get('config_address') . "\n\n";
+            $comment .= $this->language->get('text_payment') . "\n";
+          
+            $dataInfo = array(
+                'price'                 => (int)$km,
+                'to_whomName'           => $dataPriceConfirm["dataInfoOrder"]["to_whomName"],
+                'to_whomPhone'          => $dataPriceConfirm["dataInfoOrder"]["to_whomPhone"],
+                'dataWhence'            => $dataPriceConfirm["arrayDataWhence"]["whenceCity"]." ".$dataPriceConfirm["arrayDataWhence"]["whenceStreet"]." ".$dataPriceConfirm["arrayDataWhence"]["whenceHouse"]." ".$dataPriceConfirm["arrayDataWhence"]["whenceExtr"],
+                'dataWhere'             => $dataPriceConfirm["arrayDataWhere"]["whereCity"]." ".$dataPriceConfirm["arrayDataWhere"]["whereStreet"]." ".$dataPriceConfirm["arrayDataWhere"]["whereHouse"]." ".$dataPriceConfirm["arrayDataWhere"]["whereExtr"],
+                'dataWhenceCity'        => $dataPriceConfirm["arrayDataWhence"]["whenceCity"],
+                'payment_method_code'   => $dataPriceConfirm['dataInfoOrder']['payment'],
                 
+            
+            );
+            //оформляем заказ в соответствии со способом заказа
+            
+            switch($dataPriceConfirm['dataInfoOrder']['payment']){
+                
+                case "cheque":
+                    
+                   //получаем данные для оформления ордера (массив) 
+                   $order_data = $this->addOrderTaxi($dataInfo);
+                   
+                   //получили ордер id
+                   $this->session->data['order_id'] = $this->model_checkout_order->addOrder($order_data);
+                    
+                   //создаем заказ в админке
+                    $this->model_checkout_order->addOrderHistory($this->session->data['order_id'], $this->config->get('cheque_order_status_id'), $comment, true);
+                
+                
+                break;
+            
+                case "liqpay":
+                    
+                    
+                    
+                    
+                break;
+            
+            
+            //по дефолту будет идти оплата наличными
+                default:
+                    
+                
+            }
         }
         
  
@@ -272,14 +347,7 @@ class ControllerExtensionModuleTaxiAPI extends Controller {
         );
         
         
-        return $dataArrayTaxi;
-        
-        
-        #TODO надо создать switch  где ловить по классу методы оплаты и только тогда в этом же методе 
-        #осуществлять функцию оплаты по методу, если нету отплаты то ничего не делать 
-        #посмотреть API ликпея и обычной оплаты и в модели модуля сделать аналогию просто отдав данные 
-        #и по примеру сделать расчет и перенаправление и оформление заказа 
-        
+        return $dataArrayTaxi;      
     }
     
     //получаем нормальный массив (после сериализации)
@@ -344,10 +412,7 @@ class ControllerExtensionModuleTaxiAPI extends Controller {
                    $tarif = 0;
 
             }
-                
-
-	
-         
+        
          //если тариф больше 0 то считаем стоимость иначе выводит сообщение о ошибке
          if(!empty($tarif)){
              
@@ -363,6 +428,177 @@ class ControllerExtensionModuleTaxiAPI extends Controller {
 	}
 
          return $km;
+    }
+    
+    //подготавливаем данные для создания ордера и получение его id
+    public function addOrderTaxi($data){
+        
+        
+        //используется API опенкарта
+        $order_data = array();
+        
+        $order_data['invoice_prefix'] = $this->config->get('config_invoice_prefix');
+	$order_data['store_id'] = $this->config->get('config_store_id');
+	$order_data['store_name'] = $this->config->get('config_name');
+        
+        $totals = array();
+	$taxes = $this->cart->getTaxes();
+	$total = $data['price'];
+        $this->session->data['vouchers'] = $data['price'];
+
+	// Because __call can not keep var references so we put them into an array.
+	$total_data = array(
+            'totals' => &$totals,
+            'taxes'  => &$taxes,
+            'total'  => &$total
+	);
+        
+        $this->load->model('extension/extension');
+        $sort_order = array();
+        
+        $results = $this->model_extension_extension->getExtensions('total');
+
+            foreach ($results as $key => $value) {
+            	$sort_order[$key] = $this->config->get($value['code'] . '_sort_order');
+	}
+          
+        array_multisort($sort_order, SORT_ASC, $results);
+        
+        foreach ($results as $result) {
+            if ($this->config->get($result['code'] . '_status')) {
+		$this->load->model('extension/total/' . $result['code']);
+
+		// We have to put the totals in an array so that they pass by reference.
+		$this->{'model_extension_total_' . $result['code']}->getTotal($total_data);
+				}
+	}
+        
+        $sort_order = array();
+        
+        foreach ($totals as $key => $value) {
+            $sort_order[$key] = $value['sort_order'];
+	}
+        
+        array_multisort($sort_order, SORT_ASC, $totals);
+        
+        $order_data['totals'] = $totals;
+                
+        
+        if ($order_data['store_id']) {
+            $order_data['store_url'] = $this->config->get('config_url');
+		} else {
+			if ($this->request->server['HTTPS']) {
+				$order_data['store_url'] = HTTPS_SERVER;
+			} else {
+                            $order_data['store_url'] = HTTP_SERVER;
+			}
+	}
+        
+        $this->load->model('account/customer');
+        $order_data['customer_id'] = 0;
+	$order_data['customer_group_id'] = 0;
+	$order_data['firstname'] = $data["to_whomName"];
+	$order_data['lastname'] = "user";
+	$order_data['email'] = "not found";
+	$order_data['telephone'] = $data["to_whomPhone"];
+	$order_data['fax'] = 0000;
+	$order_data['custom_field'] = 0000;
+        
+        $order_data['payment_firstname'] = $data["to_whomName"];
+	$order_data['payment_lastname'] = "user";
+	$order_data['payment_company'] = "not found";
+	$order_data['payment_address_1'] = $data["dataWhence"];
+	$order_data['payment_address_2'] = $data["dataWhere"];
+	$order_data['payment_city'] = $data["dataWhenceCity"];
+	$order_data['payment_postcode'] = 0000;
+	$order_data['payment_zone'] = 0000;
+	$order_data['payment_zone_id'] = 0000;
+	$order_data['payment_country'] = "not found";
+	$order_data['payment_country_id'] = 0000;
+	$order_data['payment_address_format'] = 0000;
+	$order_data['payment_custom_field'] = 0000;
+        $order_data['affiliate_id'] = 0;
+	$order_data['commission'] = 0;
+        $order_data['marketing_id'] = 0;
+        $order_data['tracking'] = '';
+        
+        $order_data['payment_method'] = $this->language->get('checkque');
+        $order_data['payment_code'] = $data["payment_method_code"];            
+                
+            $order_data['shipping_firstname'] = '';
+            $order_data['shipping_lastname'] = '';
+            $order_data['shipping_company'] = '';
+            $order_data['shipping_address_1'] = '';
+            $order_data['shipping_address_2'] = '';
+            $order_data['shipping_city'] = '';
+            $order_data['shipping_postcode'] = '';
+            $order_data['shipping_zone'] = '';
+            $order_data['shipping_zone_id'] = '';
+            $order_data['shipping_country'] = '';
+            $order_data['shipping_country_id'] = '';
+            $order_data['shipping_address_format'] = '';
+            $order_data['shipping_custom_field'] = array();
+            $order_data['shipping_method'] = '';
+            $order_data['shipping_code'] = '';
+            
+            $option_data = array();
+            
+            $option_data[] = array(
+		'product_option_id'       => " ",
+		'product_option_value_id' => " ",
+		'option_id'               => " ",
+		'option_value_id'         => " ",
+		'name'                    => " ",
+		'value'                   => " ",
+		'type'                    => " "
+            );
+                
+
+        $order_data['products'][] = array(
+            'product_id' => substr(time(),-5),
+            'name'       => $data["dataWhence"]."-".$data["dataWhere"],
+            'model'      => "not found",
+            'option'     => $option_data,
+            'download'   => 0000,
+            'quantity'   => 1,
+            'subtract'   => 0000,
+            'price'      =>  $data["price"],
+            'total'      =>  $data["price"],
+            'tax'        => 0000,
+            'reward'     => 0000
+	);
+        
+        $order_data['comment'] = "not found";
+	$order_data['total'] = $data["price"];
+        $order_data['language_id'] = $this->config->get('config_language_id');
+                
+	$order_data['currency_id'] = $this->currency->getId($this->session->data['currency']);
+	$order_data['currency_code'] = $this->session->data['currency'];
+	$order_data['currency_value'] = $this->currency->getValue($this->session->data['currency']);
+        
+	$order_data['ip'] = $this->request->server['REMOTE_ADDR'];        
+        
+        if (!empty($this->request->server['HTTP_X_FORWARDED_FOR'])) {
+            $order_data['forwarded_ip'] = $this->request->server['HTTP_X_FORWARDED_FOR'];
+	} elseif (!empty($this->request->server['HTTP_CLIENT_IP'])) {
+            $order_data['forwarded_ip'] = $this->request->server['HTTP_CLIENT_IP'];
+	} else {
+            $order_data['forwarded_ip'] = '';
+	}
+
+	if (isset($this->request->server['HTTP_USER_AGENT'])) {
+            $order_data['user_agent'] = $this->request->server['HTTP_USER_AGENT'];
+	} else {
+            $order_data['user_agent'] = '';
+	}
+
+	if (isset($this->request->server['HTTP_ACCEPT_LANGUAGE'])) {
+            $order_data['accept_language'] = $this->request->server['HTTP_ACCEPT_LANGUAGE'];
+	} else {
+            $order_data['accept_language'] = '';
+        }
+        
+        return $order_data;
     }
 
 }
